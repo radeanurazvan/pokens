f0fd79354f44898800af990409ebdc1199f89707
Razvan <radeanu.razvan99@gmail.com>
2019-11-22 22:51:31 +0200

    Replaced identity with trainers

--- Source/Pokens.Trainers/Pokens.Trainers.Api/Program.cs
+++ Source/Pokens.Trainers/Pokens.Trainers.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Pokens.Identity.Api
+namespace Pokens.Trainers.Api
 {
     public class Program
     {
--- Source/Pokens.Trainers/Pokens.Trainers.Api/Startup.cs
+++ Source/Pokens.Trainers/Pokens.Trainers.Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Pokens.Identity.Api
+namespace Pokens.Trainers.Api
 {
     public class Startup
     {
