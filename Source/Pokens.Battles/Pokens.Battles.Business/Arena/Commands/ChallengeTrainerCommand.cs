822599a315c129053293ae6fcb1623fc97e897ce
Razvan <radeanu.razvan99@gmail.com>
2020-01-07 01:12:52 +0200

    Enriched challenges to specify pokemons

--- Source/Pokens.Battles/Pokens.Battles.Business/Arena/Commands/ChallengeTrainerCommand.cs
+++ Source/Pokens.Battles/Pokens.Battles.Business/Arena/Commands/ChallengeTrainerCommand.cs
@@ -5,17 +5,23 @@ namespace Pokens.Battles.Business
 {
     public sealed class ChallengeTrainerCommand : ICommand
     {
-        public ChallengeTrainerCommand(Guid arenaId, Guid challengerId, Guid challengedId)
+        public ChallengeTrainerCommand(Guid arenaId, Guid challengerId, Guid challengerPokemonId, Guid challengedId, Guid challengedPokemonId)
         {
             ArenaId = arenaId;
             ChallengerId = challengerId;
+            ChallengerPokemonId = challengerPokemonId;
             ChallengedId = challengedId;
+            ChallengedPokemonId = challengedPokemonId;
         }
 
         public Guid ArenaId { get; }
 
         public Guid ChallengerId { get; }
+        
+        public Guid ChallengerPokemonId { get; }
 
         public Guid ChallengedId { get; }
+
+        public Guid ChallengedPokemonId { get; }
     }
 }
---
+++ Source/Pokens.Battles/Pokens.Battles.Domain.Tests/Extensions/TrainerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace Pokens.Battles.Domain.Tests.Extensions
+{
+    public static class TrainerExtensions
+    {
+        public static Guid FirstPokemonId(this Trainer trainer) => trainer.Pokemons.First().Id;
+    }
+}
