4a08e06e67117148328f8c02644b43fa93d1effd
Razvan <radeanu.razvan99@gmail.com>
2020-01-13 21:22:25 +0200

    design changes

--- Source/Pokens/Pokens/ClientApp/src/app/modules/profile/pages/profile/profile.component.ts
+++ Source/Pokens/Pokens/ClientApp/src/app/modules/profile/pages/profile/profile.component.ts
@@ -10,10 +10,18 @@ export class ProfileComponent implements OnInit {
 
   public pokemons: any;
 
+  private palette: string[] = ['#00d793', '#C12026', '#58ABF6'];
   constructor(private profileService: ProfileService) { }
 
   ngOnInit() {
     this.profileService.getAllPokemons().subscribe(data => this.pokemons = data);
   }
 
+  getBackGroundColor(): string {
+    return this.palette[this.getRandomInt(3)];
+  }
+
+  private getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 }
