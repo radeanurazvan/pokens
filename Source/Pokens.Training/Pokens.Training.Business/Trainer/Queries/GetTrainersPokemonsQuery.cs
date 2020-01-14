6d0fa10f7e703c861e907052c108bc6d70201f37
radeanurazvan <radeanu.razvan99@gmail.com>
2020-01-14 02:54:03 +0200

    Implemented get trainers pokemon query handler (#51)

---
+++ Source/Pokens.Training/Pokens.Training.Business/Trainer/Queries/GetTrainersPokemonsQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+
+namespace Pokens.Training.Business
+{
+    public sealed class GetTrainersPokemonsQuery : IRequest<IEnumerable<PokemonModel>>
+    {
+        public GetTrainersPokemonsQuery(IEnumerable<string> trainerIds)
+        {
+            TrainerIds = trainerIds;
+        }
+
+        public IEnumerable<string> TrainerIds { get; }
+    }
+}
