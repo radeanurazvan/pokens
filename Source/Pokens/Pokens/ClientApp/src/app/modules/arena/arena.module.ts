35dc63e7d6253f4a8781316bf0adcba7487e5e8f
Razvan <radeanu.razvan99@gmail.com>
2020-03-05 01:03:52 +0200

    Added notifications to module providers

--- Source/Pokens/Pokens/ClientApp/src/app/modules/arena/arena.module.ts
+++ Source/Pokens/Pokens/ClientApp/src/app/modules/arena/arena.module.ts
@@ -7,8 +7,7 @@ import { ArenaDetailsComponent } from './pages/arena-details/arena-details.compo
 import { ChallengePopupComponent } from './pages/arena-details/challenge-popup/challenge-popup.component';
 import { ChallengesComponent } from './pages/arena/challenges/challenges.component';
 import { CurrentBattleComponent } from './pages/arena/current-battle/current-battle.component';
-
-
+import { CurrentBattleNotifications } from './pages/arena/current-battle/current-battle.notifications';
 
 @NgModule({
   declarations: [ArenaComponent, ArenaDetailsComponent, ChallengePopupComponent, ChallengesComponent, CurrentBattleComponent],
@@ -17,6 +16,7 @@ import { CurrentBattleComponent } from './pages/arena/current-battle/current-bat
     CommonModule,
     SharedModule.forChild(),
     ArenaRoutingModule
-  ]
+  ],
+  providers: [CurrentBattleNotifications]
 })
 export class ArenaModule { }
