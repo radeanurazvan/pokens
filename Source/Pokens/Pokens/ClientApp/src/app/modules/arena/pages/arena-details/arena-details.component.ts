57ab1db05a9d085b777cc75457aba73f10ee24d2
alexandrustf <37770439+alexandrustf@users.noreply.github.com>
2020-03-04 07:28:15 +0200

    Styled Arenas (#55)

--- Source/Pokens/Pokens/ClientApp/src/app/modules/arena/pages/arena-details/arena-details.component.ts
+++ Source/Pokens/Pokens/ClientApp/src/app/modules/arena/pages/arena-details/arena-details.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ArenaService } from '../../core/arena.service';
-import { ArenaModel } from '../../models/arena.model';
-import { MatDialog, MatIconRegistry } from '@angular/material';
-import { TrainerModel } from '../../models/trainer.model';
-import { ChallengePopupComponent } from './challenge-popup/challenge-popup.component';
+import { Component, OnInit } from "@angular/core";
+import { ArenaService } from "../../core/arena.service";
+import { ArenaModel } from "../../models/arena.model";
+import { MatDialog, MatIconRegistry } from "@angular/material";
+import { TrainerModel } from "../../models/trainer.model";
+import { ChallengePopupComponent } from "./challenge-popup/challenge-popup.component";
+import { UserService } from "src/app/shared/core/user.service";
 
 @Component({
-  selector: 'app-arena-details',
-  templateUrl: './arena-details.component.html',
-  styleUrls: ['./arena-details.component.scss']
+  selector: "app-arena-details",
+  templateUrl: "./arena-details.component.html",
+  styleUrls: ["./arena-details.component.scss"]
 })
 export class ArenaDetailsComponent implements OnInit {
   public arenaDetails: ArenaModel;
@@ -17,14 +18,22 @@ export class ArenaDetailsComponent implements OnInit {
 
   constructor(
     private arenaService: ArenaService,
-    private dialog: MatDialog) { }
+    private dialog: MatDialog,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.arenaService.getArenaDetails().subscribe((data: ArenaModel) => {
       this.arenaDetails = data;
+      const username = this.userService.getUserName();
+      this.arenaDetails.trainers = this.arenaDetails.trainers.filter(
+        trainer => trainer.name !== username
+      );
       this.trainersIds = this.arenaDetails.trainers.map(a => a.id);
 
-      this.arenaService.getAllPokemons(this.trainersIds).subscribe((pokemons: any) => this.pokemons = pokemons);
+      this.arenaService
+        .getAllPokemons(this.trainersIds)
+        .subscribe((pokemons: any) => (this.pokemons = pokemons));
     });
   }
 
@@ -34,8 +43,9 @@ export class ArenaDetailsComponent implements OnInit {
 
   public initChallenge(trainer: TrainerModel, pokemon: any) {
     this.dialog.open(ChallengePopupComponent, {
-      data: {arena: this.arenaDetails, trainer, pokemon},
-      width: '500px'
+      data: { arena: this.arenaDetails, trainer, pokemon },
+      width: "500px",
+      height: "680px"
     });
   }
 }
